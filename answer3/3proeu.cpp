140250c89fdd99ba03fbcc0375502ac1ef678bf3
iceweasel <johnsjosephk19@gmail.com>
2020-12-06 23:19:44 +0530

    answer3

---
+++ answer3/3proeu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+using std::cout ;
+
+int main()
+{
+int a = sqrt(600851475143);
+bool  prime[a+1]={false};
+prime[0] =prime[1]=true ;
+int
+ getchar();
+
+}
